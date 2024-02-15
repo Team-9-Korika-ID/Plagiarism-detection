438ee32e36a7809642874d304635ea12a5119f94
wahyudesu <96912274+wahyudesu@users.noreply.github.com>
2024-02-15 16:24:54 +0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 import zipfile
 import io
 
@@ -19,19 +19,23 @@ with right_column:
     st.header("Output")
     # Tampilkan output di sebelah kanan
     if uploaded_file is not None:
-        if uploaded_file.type == "application/pdf":
-            pdf_reader = PdfFileReader(uploaded_file)
-            text = ""
-            for page_num in range(pdf_reader.numPages):
-                page = pdf_reader.getPage(page_num)
-                text += page.extractText()
-            st.write("Text from PDF:")
-            st.write(text)
-        elif uploaded_file.type == "application/zip":
-            zip_file = zipfile.ZipFile(uploaded_file)
-            text = ""
-            for file_name in zip_file.namelist():
-                with zip_file.open(file_name) as file:
-                    text += file.read().decode("utf-8")
-            st.write("Text from files in ZIP:")
-            st.write(text)
+        try:
+            if uploaded_file.type == "application/pdf":
+                pdf_reader = PdfReader(uploaded_file)
+                text = ""
+                for page_num in range(len(pdf_reader.pages)):
+                    page = pdf_reader.pages[page_num]
+                    text += page.extract_text()
+                st.write("Text from PDF:")
+                st.write(text)
+            elif uploaded_file.type == "application/zip":
+                zip_file = zipfile.ZipFile(uploaded_file)
+                text = ""
+                for file_name in zip_file.namelist():
+                    if file_name.endswith('.txt'):  # Hanya membaca file teks
+                        with zip_file.open(file_name) as file:
+                            text += file.read().decode("utf-8")
+                st.write("Text from files in ZIP:")
+                st.write(text)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
